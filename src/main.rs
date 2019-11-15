625aea4819dfa8330eb35eb70c724f5eb17ecc49
Isak Jägberg <ijagberg@gmail.com>
2019-11-15 19:49:39 +0100

    Use structopt

--- src/main.rs
+++ src/main.rs
@@ -1,156 +1,73 @@
-extern crate clap;
-use clap::{App, Arg};
 use rand::prelude::*;
 use std::error::Error;
 use std::fs;
 use std::io;
 use std::io::Write;
-use std::path::PathBuf;
 use std::{thread, time};
+use structopt::StructOpt;
 
+#[derive(StructOpt)]
 struct Config {
-    difficulty: usize,
-    username: String,
+    /// Minimum length of words to display
+    ///
+    /// There is no limit by default
+    #[structopt(short, long)]
+    difficulty: Option<usize>,
+
+    /// The player's username
+    #[structopt(short, long)]
+    username: Option<String>,
+
+    /// Number of words in a round
+    #[structopt(short, long, default_value = "10")]
     word_count: usize,
-    word_list_file: PathBuf,
+
+    /// Number of words to display
+    #[structopt(long, default_value = "3")]
     display_words_count: usize,
+
+    /// File to use as source for words
+    #[structopt(long, default_value = "wordlist.txt")]
+    word_list_file: String,
 }
 
 fn main() {
-    // Handle command line stuff
-    let matches = App::new("HyperTyper")
-        .version("1.0")
-        .author("Isak Jägberg <ijagberg@gmail.com>")
-        .about("Simple command line typing game")
-        .arg(
-            Arg::with_name("difficulty")
-                .short("d")
-                .long("difficulty")
-                .help("Sets maximum length of words to display")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("username")
-                .short("u")
-                .long("username")
-                .help("Sets username to display with scores")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("word-count")
-                .short("w")
-                .long("word-count")
-                .help("Sets the number of words to type in one round")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("word-list-file")
-                .long("word-list-file")
-                .help("File containing a custom word list")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("display-words-count")
-                .long("display-words-count")
-                .help("Number of words to display")
-                .takes_value(true),
-        )
-        .get_matches();
-
-    let config = Config {
-        difficulty: match matches.value_of("difficulty") {
-            Some(d) => d
-                .parse::<usize>()
-                .expect("Could not parse integer value of argument difficulty (-d)"),
-            None => 0,
-        },
-        username: match matches.value_of("username") {
-            Some(u) => u.to_string(),
-            None => String::from(""),
-        },
-        word_count: match matches.value_of("word-count") {
-            Some(w) => w
-                .parse::<usize>()
-                .expect("Could not parse integer value of argument word-count (-w)"),
-            None => 15,
-        },
-        word_list_file: match matches.value_of("word-list-file") {
-            Some(path_string) => PathBuf::from(path_string),
-            None => {
-                let mut path = PathBuf::new();
-                path.push("wordlist.txt");
-                path
-            }
-        },
-        display_words_count: match matches.value_of("display-words-count") {
-            Some(i) => i
-                .parse::<usize>()
-                .expect("Could not parse integer value of argument display-words-count"),
-            None => 3,
-        },
-    };
+    let config = Config::from_args();
 
     start_game(config);
 }
 
 fn start_game(config: Config) {
     // Get wordlist from file and split into vector
-    let contents = fs::read_to_string(&config.word_list_file).expect("Could not read file!");
-    let words = match get_words(&contents, &config) {
-        Ok(words) => {
-            if words.len() >= config.display_words_count {
-                words
-            } else {
-                eprintln!(
-                    "Fewer than {} words in file {}/{}",
-                    config.display_words_count,
-                    config.word_list_file.parent().unwrap().to_str().unwrap(),
-                    config.word_list_file.file_name().unwrap().to_str().unwrap(),
-                );
-                return;
-            }
-        }
-        _error => {
-            eprintln!(
-                "Could not get words from {}/{}",
-                config.word_list_file.parent().unwrap().to_str().unwrap(),
-                config.word_list_file.file_name().unwrap().to_str().unwrap(),
-            );
-            return;
-        }
-    };
+    let contents = fs::read_to_string(&config.word_list_file).expect("could not read file");
+    let words = get_words(&contents, &config);
 
     // Print welcoming message and countdown
     print_countdown().expect("Could not print countdown message");
 
     match run(&config, words) {
-        Ok(elapsed) => {
-            if config.username.is_empty() {
-                println!("Time: {:?}", elapsed);
-            } else {
-                println!("Time: {:?} for user: {}", elapsed, config.username);
-            }
-        }
-        Err(_) => eprintln!("Some error occurred!"),
+        Ok(elapsed) => match config.username {
+            None => println!("Time: {:?}", elapsed),
+            Some(username) => println!("Time: {:?} for user: {}", elapsed, username),
+        },
+        Err(_) => eprintln!("Some error occurred"),
     }
 }
 
-fn get_words<'a>(contents: &'a str, config: &Config) -> Result<Vec<&'a str>, Box<dyn Error>> {
+fn get_words<'a>(contents: &'a str, config: &Config) -> Vec<&'a str> {
     let mut words = Vec::new();
     let mut rng = rand::thread_rng();
 
     for line in contents.lines() {
-        if config.difficulty == 0 || line.len() <= config.difficulty {
+        if config.difficulty.is_none() || line.len() <= config.difficulty.unwrap() {
             words.push(line);
         }
     }
     words.shuffle(&mut rng);
-
-    Ok(words)
+    words
 }
 
-fn print_countdown() -> Result<(), Box<Error>> {
+fn print_countdown() -> Result<(), Box<dyn Error>> {
     println!("Welcome to HyperTyper!");
 
     let mut seconds_left = 3;
@@ -194,7 +111,7 @@ fn run(config: &Config, words: Vec<&str>) -> Result<std::time::Duration, io::Err
 
         // iterate over display words to check for a match
         for (index, display_word) in display_words.iter().enumerate() {
-            if display_word.eq(&user_input) {
+            if display_word == &user_input {
                 written_words += 1;
                 display_words[index] = words[next_word_index];
                 next_word_index += 1;
